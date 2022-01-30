e072f98cd6da7b376cc584435ef5082cbf9d407b
Dubrov Ruslan <dubrov.ruslan@gmail.com>
2022-01-30 19:11:20 +0200

    Added Entities

---
+++ Module4HW6/Entities/Artist.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Module4HW6.Entities
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string InstagramUrl { get; set; }
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+    }
+}
---
+++ Module4HW6/Entities/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Module4HW6.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ICollection<Song> Songs { get; set; } = new List<Song>();
+    }
+}
