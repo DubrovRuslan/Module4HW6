ce0e60be1b5416875d7789e16bfeeeffcaf6abe1
Dubrov Ruslan <dubrov.ruslan@gmail.com>
2022-01-30 19:47:56 +0200

    Added EntityConfigurations

---
+++ Module4HW6/EntityConfigurations/GenreConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Module4HW6.Entities;
+
+namespace Module4HW6.EntityConfigurations
+{
+    public class GenreConfiguration : IEntityTypeConfiguration<Genre>
+    {
+        public void Configure(EntityTypeBuilder<Genre> builder)
+        {
+            builder.ToTable("Genre").HasKey(g => g.Id);
+            builder.Property(g => g.Title).IsRequired().HasMaxLength(100);
+        }
+    }
+}
